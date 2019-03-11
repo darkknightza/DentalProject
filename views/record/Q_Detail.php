cead4f6902282ea98bd27265f8b19a4c5d512d06
nattakit325 <nayangklukcity@gmil.com>
2019-03-11 11:45:07 +0700

    select

--- views/record/Q_Detail.php
+++ views/record/Q_Detail.php
@@ -1,6 +1,11 @@
 <!-- about section -->
 
 <link rel="stylesheet" href="/public/css/jquery.inputpicker.css" />
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 <script src="/public/js/jquery.inputpicker.js"></script>
 <div class="row">
     <div class="contact-caption clearfix">
@@ -15,29 +20,37 @@
                     <p align="left" ><font size="4" color="white">วันที่นัดหมาย</font></p><input type="datetime-local" name="bdaytime" value="<?php echo date('Y-m-d\TH:i', strtotime($data['Q_Detail']['time'])); ?>">
                     <p align="left" ><font size="4" color="white">หมายเหตุ</font></p> 
                     <input value="<?php echo $data['Q_Detail']['detail']; ?>" type="text"  name="detail" >
-                    <p align="left" ><font size="4" color="white">สถานะการนัดหมาย</font></p> <input id="test" value="<?php echo $data['Q_Detail']['status'] ?>" name="status"/>
+                    <p align="left" ><font size="4" color="white">สถานะการนัดหมาย</font></p> 
+                    <select value="<?php echo $data['Q_Detail']['status'] ?>" name="status" align="left" class="form-control">
+                        <option value="1">ยังไม่มา</option>
+                        <option value="2">รอพบแพทย์</option>
+                        <option value="3">รอจ่ายเงิน</option>
+                        <option value="4">ยกเลิกนัด</option>
+                        <option value="5">เสร็จสิ้น</option>
+                    </select>
+                    <br>
                     <input class="submit-btn" type="submit" value="บันทึก">
                 </form>
         </div>
     </div>
 </div>
 <script>
-$('#test').inputpicker({
-    data:[
-        {value:"<?php echo $data['Q_Detail']['status_id'] ?>",text:"<?php echo $data['Q_Detail']['status'] ?>"},
-    <?php foreach($data['Q_Status'] as $row){ ?>
-        <?php if($data['Q_Detail']['status_id'] == $row['status_id']){
-        }else{ ?>
-        {value:"<?php echo $row['status_id'] ?>",text:"<?php echo $row['status_name'] ?>"},
-        <?php }} ?>
-        {}
-    ],
-    fields:['value','text'],
-    fieldText : 'text',
-    headShow: true,
-    filterOpen: true,
-    autoOpen: true
-});
+// $('#test').inputpicker({
+//     data:[
+//         {value:"<?php echo $data['Q_Detail']['status_id'] ?>",text:"<?php echo $data['Q_Detail']['status'] ?>"},
+//     <?php foreach($data['Q_Status'] as $row){ ?>
+//         <?php if($data['Q_Detail']['status_id'] == $row['status_id']){
+//         }else{ ?>
+//         {value:"<?php echo $row['status_id'] ?>",text:"<?php echo $row['status_name'] ?>"},
+//         <?php }} ?>
+//         {}
+//     ],
+//     fields:['value','text'],
+//     fieldText : 'text',
+//     headShow: true,
+//     filterOpen: true,
+//     autoOpen: true
+// });
 
 $('#test2').inputpicker({
     data:[
