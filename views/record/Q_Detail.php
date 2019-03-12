f2301afc4ae4fe72694c24861d4bbc1fe9e095c6
nattakit325 <nayangklukcity@gmil.com>
2019-03-12 23:34:03 +0700

    menu

--- views/record/Q_Detail.php
+++ views/record/Q_Detail.php
@@ -3,7 +3,6 @@
 <link rel="stylesheet" href="/public/css/jquery.inputpicker.css" />
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 <script src="/public/js/jquery.inputpicker.js"></script>
