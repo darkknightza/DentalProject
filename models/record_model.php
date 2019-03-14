816164c3a261a6456ca0e781d2047d0f7232a482
nattakit325 <nayangklukcity@gmil.com>
2019-03-14 16:31:09 +0700

    d

--- models/record_model.php
+++ models/record_model.php
@@ -122,7 +122,7 @@ class record_model extends Model
     }
     public function GetAllQ(){
 
-        $sql ="SELECT p.patient_name as patientName,u.name as dentist,us.name as UpdateBy,t.Treatment_q_time as time,t.detail as detail,t.Time_arrive as arrive,s.status_name as status,s.color as color,t.status_id as status_id, t.treatment_Q_id as t_id FROM treatment_q t INNER JOIN patient p on t.patient_id = p.patient_id INNER join user u on t.dentist_id = u.user_id INNER join user us on t.UpdateBy = us.user_id INNER join status s on t.status_id=s.status_id WHERE t.Time_arrive > NOW() - INTERVAL 3 DAY or (t.status_id != 4 and t.status_id != 5)   ORDER by t.Treatment_q_time";
+        $sql ="SELECT p.patient_name as patientName,u.name as dentist,us.name as UpdateBy,t.Treatment_q_time as time,t.detail as detail,t.Time_arrive as arrive,s.status_name as status,s.color as color,t.status_id as status_id, t.treatment_Q_id as t_id FROM treatment_q t INNER JOIN patient p on t.patient_id = p.patient_id INNER join user u on t.dentist_id = u.user_id INNER join user us on t.UpdateBy = us.user_id INNER join status s on t.status_id=s.status_id WHERE t.status_id != 4 and t.status_id != 5   ORDER by t.Treatment_q_time";
 
         $pstm = $this->connect->prepare($sql);
         $pstm->execute();
